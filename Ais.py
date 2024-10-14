7f0731fd971e7fb67c12f660180b8a57233c1c12
BUBOcalle <calle.dahlin@live.se>
2024-10-14 19:19:43 +0200

    Created stateOfGame class

--- Ais.py
+++ Ais.py
@@ -1,8 +1,14 @@
 from rollplayClasses import *
 import google.generativeai as genai
 from google.generativeai.types import HarmCategory, HarmBlockThreshold
+import os
+from dotenv import load_dotenv
 
-class storyTeller:
+load_dotenv()
+genai.configure(api_key=os.environ["API_KEY"])
+
+
+class stateOfTheGame():
     def __init__(self):
         self.hero = Hero(type="hero", name="Calle Dachalin", hp=10, battleSkill=6, location="the tavern", description="a tall blonde man with a destiny", equipment=["his favourite book", "flint and steel", "some beer"], weapons=[Weapon(3, "Mighty stick")])
         self.enemiesInScenes = {"the tavern":[]}
@@ -11,28 +17,54 @@ class storyTeller:
         self.NPCs = []
         self.model = genai.GenerativeModel(
                     "gemini-1.5-flash",
+                    system_instruction="""You will give information to the DM of a game, the information is not for the player""")
+        self.stringRepresentationOfData = f"""Hero: {self.hero},
+                                            Location: {self.hero.location},
+                                            Enemies in scene: {self.enemiesInScenes},
+                                            NPCs in scene: {self.NPCInScenes},
+                                            """
+    def __str__(self) -> str:
+        return  self.model.generate_content(["Given the following data explain the state of the game", self.stringRepresentationOfData],
+                                        generation_config=genai.types.GenerationConfig(
+                                        max_output_tokens=1000,
+                                        temperature=0.0,
+                                        top_p=0.95
+                                        ),
+                                        safety_settings={
+                                                HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_NONE,
+                                                HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_NONE,
+                                                HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: HarmBlockThreshold.BLOCK_NONE,
+                                                HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE,
+                                            }).text
+   
+        
+
+class storyTeller:
+    def __init__(self):
+        self.model = genai.GenerativeModel(
+                    "gemini-1.5-flash",
                     system_instruction="""You are the dungeon master in a DnD campaign. Make sure to keep track of where the player and are charakters are at all time!""")
         pass
     
     def __str__(self):
         pass
 
-    def createPromt(self, inputs, history, currentLocation):
+    def createPromt(self, inputs, history, stateOfTheGame):
         return f"""
             <instructions>
                 Be a good dugeon master and create an epic story. Tell what happens next in the story
             <\instructions>
 
             <Hero character>
-                {self.hero}
+                {stateOfTheGame.hero}
             <\Hero character>
 
             <Enemies in scene>
-                {self.enemiesInScenes[currentLocation]}
+                {stateOfTheGame.enemiesInScenes[stateOfTheGame.hero.location]}
             <\Enemies in scene>
 
             <NPC:s in scene>
-                {self.enemiesInScenes[currentLocation]}
+                {stateOfTheGame.enemiesInScenes[stateOfTheGame.hero.location]}
             <\\NPC:s in scene>
 
             <history of conversation>
@@ -75,3 +107,8 @@ class infoFetcherAi:
                                         temperature=0.0,
                                         top_p=0.95
                                         )).text)
+        
+        
+
+SotG = stateOfTheGame()
+print(SotG)
